90ba6da62876d864e2c664f7a43e399d432de0a0
tataqr <tata_98@ukr.net>
2022-04-27 09:32:51 +0300

    Adding Our State

--- Waves Music App/react-player/src/App.js
+++ Waves Music App/react-player/src/App.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
 import data from './util';
 import './styles/app.scss';
 
-
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
