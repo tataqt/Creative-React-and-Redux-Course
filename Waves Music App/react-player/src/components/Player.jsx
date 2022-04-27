98c616497fb3036d5a3327cb09bd18418da1bb87
tataqr <tata_98@ukr.net>
2022-04-27 09:47:56 +0300

    add play and pause

--- Waves Music App/react-player/src/components/Player.jsx
+++ Waves Music App/react-player/src/components/Player.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+    const audioRef = useRef(null);
+
+    const playSongHandler = () => {
+        isPlaying ? audioRef.current.pause() : audioRef.current.play();
+        setIsPlaying(!isPlaying)
+    }
+
     return (
         <div className='player-container'>
             <div className="time-control">
@@ -12,9 +19,10 @@ const Player = () => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
-                <FontAwesomeIcon className='play' size='2x' icon={faPlay} />
+                <FontAwesomeIcon className='play' size='2x' icon={faPlay} onClick={playSongHandler} />
                 <FontAwesomeIcon className='skip-forward' size='2x' icon={faAngleRight} />
             </div>
+            <audio src={currentSong.audio} ref={audioRef} />
         </div>
 
     );
