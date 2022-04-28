a9d20228ae5781c4c25fa00ea159a19c21ddea27
tataqr <tata_98@ukr.net>
2022-04-28 13:12:31 +0300

    add toggle to play pause

--- Waves Music App/react-player/src/components/Player.jsx
+++ Waves Music App/react-player/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faAngleLeft, faAngleRight, faPause } from '@fortawesome/free-solid-svg-icons';
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     const audioRef = useRef(null);
@@ -37,7 +37,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
-                <FontAwesomeIcon className='play' size='2x' icon={faPlay} onClick={playSongHandler} />
+                <FontAwesomeIcon className='play' size='2x' icon={isPlaying ? faPause : faPlay} onClick={playSongHandler} />
                 <FontAwesomeIcon className='skip-forward' size='2x' icon={faAngleRight} />
             </div>
             <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} onChange={dragHandler} src={currentSong.audio} ref={audioRef} />
