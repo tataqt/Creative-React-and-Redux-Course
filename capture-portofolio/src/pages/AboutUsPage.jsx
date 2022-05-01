84c7c0e0e884ceb199047b0d84992ac33cbbd062
tataqt <tata_98@ukr.net>
2022-05-01 12:56:04 +0300

    Faq Section

--- capture-portofolio/src/pages/AboutUsPage.jsx
+++ capture-portofolio/src/pages/AboutUsPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutUsPage = () => {
     return (
         <>
             <AboutSection />
             <ServicesSection />
+            <FaqSection />
         </>
     );
 };
