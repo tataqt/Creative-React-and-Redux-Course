80a3444e345837ad1d67c3558cb1cf485b2c9a86
tataqt <tata_98@ukr.net>
2022-05-06 11:08:58 +0300

    faq toggle & anim

--- capture-portofolio/src/components/FaqSection.jsx
+++ capture-portofolio/src/components/FaqSection.jsx
@@ -1,43 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AnimateSharedLayout } from 'framer-motion';
 import { About } from '../styles';
+import ToggleFAQ from './Toggle';
 
 const FaqSection = () => {
     return (
         <FAQ>
             <h2>Any questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How do i start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Differnt payment methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <AnimateSharedLayout>
+                <ToggleFAQ title='How do i start?'>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </ToggleFAQ>
+                <ToggleFAQ title='Daily schedule'>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis?</p>
+                    </div>
+                </ToggleFAQ>
+                <ToggleFAQ title='Differnt payment methods'>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis?</p>
+                    </div>
+                </ToggleFAQ>
+                <ToggleFAQ title='What products do you offer?'>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis?</p>
+                    </div>
+                </ToggleFAQ>
+            </AnimateSharedLayout>
         </FAQ>
     );
 };
