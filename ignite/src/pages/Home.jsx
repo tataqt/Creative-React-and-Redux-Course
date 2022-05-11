c471da1f84f96aa331c9564b64e4335871cebf82
tataqt <tata_98@ukr.net>
2022-05-11 11:45:49 +0300

    Styling Up The Cards

--- ignite/src/pages/Home.jsx
+++ ignite/src/pages/Home.jsx
@@ -1,20 +1,50 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import Game from '../components/Game';
 import { loadGames } from '../redux/actions/gamesActions';
 
 const Home = () => {
     const dispatch = useDispatch();
+    const { popular, newGames, upcoming } = useSelector(state => state.games);
 
     useEffect(() => {
         dispatch(loadGames());
         // eslint-disable-next-line
-    }, []);
+    }, [dispatch]);
 
     return (
-        <div>
-            Hello ignite
-        </div>
+        <GameList>
+            <h2>Upcoming games</h2>
+            <Games>
+                {upcoming.map(game =>
+                    <Game
+                        key={game.id}
+                        id={game.id}
+                        name={game.name}
+                        released={game.released}
+                        image={game.background_image}
+                    />
+                )}
+            </Games>
+        </GameList>
     );
 };
+
+const GameList = styled(motion.div)`
+    padding: 0rem 5rem;
+    h2{
+        padding: 5rem 0;
+    }
+`;
+
+const Games = styled(motion.div)`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px,1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem;
+`;
 
 export default Home;
