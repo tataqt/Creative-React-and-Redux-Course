bc51d32785f754963f3687bef7a632fec513a530
tataqt <tata_98@ukr.net>
2022-05-12 12:02:31 +0300

    Game Detail Styling

--- ignite/src/components/GameDetail.jsx
+++ ignite/src/components/GameDetail.jsx
@@ -13,26 +13,26 @@ const GameDetail = () => {
     return (
         <CardShadow>
             <Detail>
-                <div className="stats">
+                <Stats>
                     <div className="rating">
                         <h3>{detail.name}</h3>
                         <p>Rating: {detail.rating}</p>
                     </div>
-                    <div className="info">
+                    <Info>
                         <h3>Platform</h3>
-                        <div className="platforms">
+                        <Platforms>
                             {detail.platforms.map(data =>
                                 <h3 key={data.platform.id}>{data.platform.name}</h3>
                             )}
-                        </div>
-                    </div>
-                </div>
-                <div className="media">
+                        </Platforms>
+                    </Info>
+                </Stats>
+                <Media>
                     <img src={detail.background_image} alt="game" />
-                </div>
-                <div className="description">
+                </Media>
+                <Description>
                     <p>{detail.description_raw}</p>
-                </div>
+                </Description>
                 <div className="galary">
                     {detail.screenshots.map(screen =>
                         <img key={screen.id} src={screen.image} alt='game' />
@@ -65,7 +65,7 @@ const CardShadow = styled(motion.div)`
 const Detail = styled(motion.div)`
   width: 80%;
   border-radius: 1rem;
-  padding: 2rem 20rem;
+  padding: 2rem 5rem;
   background: #fff;
   position: absolute;
   left: 10%;
@@ -74,5 +74,35 @@ const Detail = styled(motion.div)`
       width: 100%;
   }
 `;
+
+const Stats = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Info = styled(motion.div)`
+    text-align: center;
+`;
+
+const Platforms = styled(motion.div)`
+  display: flex;
+  justify-content: space-evenly;
+  img{
+      margin-left: 3rem;
+  }
+`;
+
+const Media = styled(motion.div)`
+    margin-top: 5rem;
+    img{
+        width: 100%;
+        object-fit: cover;
+    }
+`;
+
+const Description = styled(motion.div)`
+    margin: 5rem 0;
+`
 
 export default GameDetail;
